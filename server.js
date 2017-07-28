76c239101535b25df1da06b72f8cd55239ecabc0
Abhay Saxena <ark3@datawire.io>
2017-07-28 16:55:42 -0400

    Add explanatory and copyright comments

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
+/*
+ * Copyright 2017 Datawire. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Example auth service for Ambassador[1] using ExtAuth[2].
+ * See the Ambassador documentation[3] for more information.
+ * [1]: https://github.com/datawire/ambassador
+ * [2]: https://github.com/datawire/ambassador-envoy
+ * [3]: http://www.getambassador.io/
+*/
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
